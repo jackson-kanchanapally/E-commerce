1ab45264fa817d73c7a0672c0dfcf2b12d0b1347
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-10-27 21:32:58 +0530

    added backend

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,31 @@
 import React from 'react'
-
-export default function Home() {
+import {HeroBanner, LayOut, Product, FooterBanner, NavBar, Footer} from '../components'
+import {client} from '../lib/client'
+export default function Home({products,bannerData}) {
   return (
 <>
-  herobanner
-  <div>
+  <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+  {/* {console.log(bannerData)} */}
+  <div className='products-heading'>
     <h1>Best Selling products</h1>
     <p>Speakers of many variations</p>
   </div>
+ 
   <div>
-    {['p1','p2'].map((p)=>p)}
+    {products?.map((p)=>p.name)}
   </div>
+  <Footer/>
 </>
   )
+}
+export const getServerSideProps=async()=>{
+  const query='*[_type=="product"]';
+  const products=await client.fetch(query);
+
+  const bannerQuery='*[_type=="banner"]';
+  const bannerData=await client.fetch(bannerQuery);
+
+  return{
+    props:{products,bannerData}
+  }
 }
